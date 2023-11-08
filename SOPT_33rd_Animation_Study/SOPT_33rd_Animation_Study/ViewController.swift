481b6d8a247d31c85b6b25d99b3aae5caa88396f
chaentopia <cce523@hanmail.net>
2023-11-08 14:12:10 +0900

    setting: project setting

---
+++ SOPT_33rd_Animation_Study/SOPT_33rd_Animation_Study/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SOPT_33rd_Animation_Study
+//
+//  Created by 정채은 on 11/8/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
